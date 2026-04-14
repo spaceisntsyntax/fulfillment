cd79c1f969b0e0bcf836f1bdb7995511e746c27c
Mike Rylander <mrylander@gmail.com>
2026-04-14 10:28:20 -0400

    Update documentation links
    Signed-off-by: Mike Rylander <mrylander@gmail.com>

--- Open-ILS/src/sql/Pg/950.data.seed-values.sql
+++ Open-ILS/src/sql/Pg/950.data.seed-values.sql
@@ -23849,9 +23849,8 @@ VALUES
 ,   ( 5, 2, 0, 'header',        oils_i18n_gettext( 5, 'Bibliographic Search', 'cusppe', 'label'), NULL, NULL, NULL, 1)
 ,   ( 6, 2, 1, 'catalogsearch', oils_i18n_gettext( 6, 'Search Catalog', 'cusppe', 'label'), NULL, NULL, NULL, 1)
 ,   ( 9, 3, 0, 'header',        oils_i18n_gettext( 9, 'Administration', 'cusppe', 'label'), NULL, NULL, NULL, 1)
-,   (10, 3, 1, 'link',          oils_i18n_gettext(10, 'Fulfillment Documentation', 'cusppe', 'label'), '/images/portal/helpdesk.png', 'https://docs.evergreen-ils.org', TRUE, 1)
+,   (10, 3, 1, 'link',          oils_i18n_gettext(10, 'Fulfillment Documentation', 'cusppe', 'label'), '/images/portal/helpdesk.png', '/fulfillment-ill-documentation.pdf', TRUE, 1)
 ,   (11, 3, 2, 'menuitem',      oils_i18n_gettext(11, 'Workstation Administration', 'cusppe', 'label'), '/images/portal/helpdesk.png', '/eg/staff/admin/workstation/index', NULL, 1)
-,   (12, 3, 3, 'menuitem',      oils_i18n_gettext(12, 'Reports', 'cusppe', 'label'), '/images/portal/reports.png', '/eg2/staff/reporter/full', NULL, 1)
 ;
 
 SELECT setval('config.ui_staff_portal_page_entry_id_seq', 100);
--- Open-ILS/src/sql/Pg/version-upgrade/FF-11.0-12.0-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/FF-11.0-12.0-upgrade-db.sql
@@ -19421,7 +19421,7 @@ VALUES
 ,   ( 5, 2, 0, 'header',        oils_i18n_gettext( 5, 'Bibliographic Search', 'cusppe', 'label'), NULL, NULL, NULL, 1)
 ,   ( 6, 2, 1, 'catalogsearch', oils_i18n_gettext( 6, 'Search Catalog', 'cusppe', 'label'), NULL, NULL, NULL, 1)
 ,   ( 9, 3, 0, 'header',        oils_i18n_gettext( 9, 'Administration', 'cusppe', 'label'), NULL, NULL, NULL, 1)
-,   (10, 3, 1, 'link',          oils_i18n_gettext(10, 'Fulfillment Documentation', 'cusppe', 'label'), '/images/portal/helpdesk.png', 'https://docs.evergreen-ils.org', TRUE, 1)
+,   (10, 3, 1, 'link',          oils_i18n_gettext(10, 'Fulfillment Documentation', 'cusppe', 'label'), '/images/portal/helpdesk.png', '/fulfillment-ill-documentation.pdf', TRUE, 1)
 ,   (11, 3, 2, 'menuitem',      oils_i18n_gettext(11, 'Workstation Administration', 'cusppe', 'label'), '/images/portal/helpdesk.png', '/eg/staff/admin/workstation/index', NULL, 1)
 ;
 
